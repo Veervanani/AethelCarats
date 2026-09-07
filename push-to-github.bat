dd9db1fcb77314f4131323c65d12cf1c08ebd2b1
aethelcarats <aethelcarats1@gmail.com>
2026-09-08 01:14:59 +0530

    chore: add github push helper script

---
+++ push-to-github.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ========================================================
+echo Pushing AethelCarats platform to GitHub repository...
+echo Remote: https://github.com/aethelcarats/AethelCarats.git
+echo ========================================================
+git push -u origin main
+pause
